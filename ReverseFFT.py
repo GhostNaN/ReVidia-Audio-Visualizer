2d3b3e93c9aa8b428d8a024550b07c14e0317e54
GhostNaN <59353890+GhostNaN@users.noreply.github.com>
2020-05-19 14:47:27 -0400

    Made GUI modifications and added a Reverse FFT
    - The old GUI was falling apart after updating PyQt5 so I reworked the GUI into grids and buttons.
    - Added what I'm calling a "Reverse FFT" which takes the output of the FFT(frequency plots) back into sound with sine waves.
    I made it in a way to get a cleaner sound so it's not 100% accurate on purpose.

---
+++ ReverseFFT.py
@@ -0,0 +1,72 @@
+#!venv2/bin/python
+
+import ReVidia
+import numpy as np
+import wave
+
+
+def start(file, sampleRate, barValues, time, height, oldVolList, oldTimes, freqList):
+
+    volList = list(map(lambda plot: plot / height, barValues))
+
+    waves, endTimes = createWaves(sampleRate, freqList, volList, oldVolList, time, oldTimes)
+
+    file.writeframes(waves)
+
+    return (volList, endTimes)
+
+
+def createFile(sampleRate):
+
+    file = wave.open('reverseFFT.wav', 'wb')
+    file.setframerate(sampleRate)
+    file.setnchannels(1)
+    file.setsampwidth(2)
+
+    return file
+
+
+def createWaves(sampleRate, freqPlot, volList, oldVolList, time, oldTimes):
+
+    sampleList = []
+    endTimes = []
+    for i in range(len(freqPlot)):
+        vol = volList[i]
+        freq = freqPlot[i]
+        if 0 < freq:
+            # Sync up old to new wave to avoid clipping sounds
+            if len(oldTimes) == len(freqPlot):
+                startTime = oldTimes[i] % (1/freq)
+            else:
+                startTime = 0
+            endTime = startTime + time
+            endTimes.append(endTime)
+
+            samples = np.linspace(startTime, endTime, int(sampleRate*time), endpoint=False)
+            signal = np.sin(2 * np.pi * freq * samples)
+
+            # Transition from old volume to new nicely
+            if len(oldVolList) == len(volList):
+                oldVol = oldVolList[i]
+                tranSamples = int(sampleRate * time * 0.10)
+
+                tranVol = np.linspace(oldVol, vol, tranSamples)
+                tranSignal = np.multiply(signal[:tranSamples], tranVol)
+                newSignal = signal[tranSamples:] * vol
+
+                finalSignal = np.concatenate((tranSignal, newSignal)) * 32767
+            else:
+                finalSignal = signal * (vol * 32767)
+
+            sampleList.append(finalSignal)
+        else:
+            endTimes.append(0)
+
+    combinedWave = sampleList[0]
+    for i in range(1, len(sampleList)):
+        combinedWave = np.add(combinedWave, sampleList[i])
+
+    finalWave = combinedWave / len(sampleList)
+    finalWave = finalWave.astype('int16')
+
+    return (finalWave, endTimes)
